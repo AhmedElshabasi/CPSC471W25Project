0080da6e2eb9c8f5a7904a169fc9f88e3e987c53
AhmedElshabasi <ahmed.elshabasi@ucalgary.ca>
2025-04-03 13:19:46 -0600

    Initialized users page and its route in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UsersPage from "./components/ui/UsersPage/UsersPage";
+import { useState } from 'react'
+import './App.css'
 import Header from "/src/components/ui/header"
 
 function App() {
 
   return (
+    <>
+       <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/users" element={<UsersPage />} />
+      </Routes>
+    </Router>
     <Header></Header>
+    </>
+    
   )
 }
 export default App
