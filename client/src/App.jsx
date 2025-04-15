bb5fcf329c19a85c73bd96f2ccbea3d651e83bd0
Natesh Oad <natesh.oad@ucalgary.ca>
2025-04-15 00:40:51 -0600

    Hooked up admin page to the home page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UsersPage from "./components/ui/UsersPage/UsersPage";
 import SignUpPage from "./components/ui/SignUpPage/SignUpPage";
 import Home from "./components/ui/HomePage/HomePage";
+import AdminPage from "./components/ui/AdminPage/AdminPage";
 import { useState } from 'react'
 import './App.css'
 import Header from "/src/components/ui/header"
@@ -16,7 +17,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path="/users" element={<UsersPage />} />
           <Route path="/signUp" element={<SignUpPage />} />
-          {/**<Route path="/admin" element={<AdminPage />} /> **/}
+          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </Router>
     </div>
