911d61704f9dded9eca538ea33afe9f8021c4693
Anmoldeep Gill <anmoldeep.gill@ucalgary.ca>
2025-04-14 21:20:35 -0600

    Designed Admin Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UsersPage from "./components/ui/UsersPage/UsersPage";
 import SignUpPage from "./components/ui/SignUpPage/SignUpPage";
-import { useState } from 'react'
-import './App.css'
-import Header from "/src/components/ui/header"
+import { useState } from "react";
+import "./App.css";
+import Header from "/src/components/ui/header";
+import AdminPage from "./components/ui/AdminPage/AdminPage";
 
 function App() {
-
   return (
     <>
-    <div className="dark bg-background text-foreground min-h-screen">
-      <Router>
-      <Header></Header>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route path="/users" element={<UsersPage />} />
-          <Route path="/signUp" element={<SignUpPage />} />
-        </Routes>
-      </Router>
-    </div>
+      <div className="dark bg-background text-foreground min-h-screen">
+        <Router>
+          <Header></Header>
+          <Routes>
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/users" element={<UsersPage />} />
+            <Route path="/signUp" element={<SignUpPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+          </Routes>
+        </Router>
+      </div>
     </>
-    
-  )
+  );
 }
-export default App
+export default App;
