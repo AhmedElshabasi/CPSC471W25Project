72ce198a135226e39e9f2e760732db787d9482d6
Anmoldeep Gill <anmoldeep.gill@ucalgary.ca>
2025-04-15 22:40:11 -0600

    Retrieve movie data from search bar functionality.

--- client/src/components/ui/header.jsx
+++ client/src/components/ui/header.jsx
@@ -7,15 +7,57 @@ import { Film } from "lucide-react";
 function Header() {
   const [search, setSearch] = useState("");
 
+  const searchMovie = async (e) => {
+    if (e.key === "Enter" && search.trim()) {
+      try {
+        const response = await fetch("http://localhost:3001/api/movies/movie", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ movieName: search }),
+        });
+
+        if (!response.ok) {
+          console.log("Movie not found.");
+          return;
+        }
+
+        const data = await response.json();
+        alert(data.rows[0].name);
+      } catch (error) {
+        console.error("Error finding movie:", error.message);
+      }
+    }
+  };
+
+  // const retrieveMovie = async (e) => {
+  //   if (e.key === "Enter" && search.trim()) {
+  //       try {
+  //           const response = await fetch("http://localhost:3001/api/movies/movies");
+  //           const movies = await response.json();
+
+  //         } catch (error) {
+  //           console.error("Error finding movie:", error.message);
+  //         }
+  //   }
+  // };
+
   return (
     <div className="flex w-full bg-black h-[65px]">
       <h1 className="flex w-full h-full justify-start items-center font-bold ml-10 text-xl">
         <Link to="/" className="flex items-center gap-2">
-        <Film></Film>SceneBook
+          <Film></Film>SceneBook
         </Link>
-      </h1 >
+      </h1>
       <div className="flex w-full h-full justify-center items-center">
-        <Input type="" placeholder="Search for Movies" className="h-[60%] my-[10px]"></Input>
+        <Input
+          type=""
+          placeholder="Search for Movies"
+          className="h-[60%] my-[10px]"
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={searchMovie}
+        ></Input>
       </div>
       <div className="flex w-full h-full justify-end items-center gap-4 mr-10">
         <Link to="/signup">
--- server/controllers/movies.js
+++ server/controllers/movies.js
@@ -1,17 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
-import client from "../index.js"
+import client from "../index.js";
 
 const movieRouter = express.Router();
 
 // Set up a router for GET requests on /api/movies/movies
 movieRouter.get("/movies", async (req, res) => {
   try {
-    const response = await client.query(`SELECT * FROM MOVIE`)
-    res.json(response)
-    
+    const response = await client.query(`SELECT * FROM MOVIE`);
+    res.json(response);
   } catch (error) {
     res.status(500).json({ error: "No movies in the database." });
+  }
+});
+
+movieRouter.post("/movie", async (req, res) => {
+  const { movieName } = req.body;
+  try {
+    const result = await client.query(`SELECT * FROM MOVIE WHERE Name = $1`, [
+      movieName,
+    ]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "No such movie in database." });
+    }
+
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Could not retrieve movie from database." });
   }
 });
 
